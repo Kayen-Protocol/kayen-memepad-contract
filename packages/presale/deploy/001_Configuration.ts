b90e51565eb7e6714f8d5e26595f3785c1dcb72f
boxfox619 <boxfoxsg619@gmail.com>
2024-08-06 10:34:01 +0900

    Redeploy

--- packages/presale/deploy/001_Configuration.ts
+++ packages/presale/deploy/001_Configuration.ts
@@ -1,17 +1,23 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-
-const FEE_VAULT_ADDRESS = "0xb9dF4BD9d3103cF1FB184BF5e6b54Cf55de81747";
+import { FEE_VAULT_ADDRESS, WCHZ } from "./constants";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
+  const { deployments, getNamedAccounts, ethers } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
+  const signer = await ethers.getSigner(deployer);
 
   const configuration = await deploy("Configuration", {
     from: deployer,
     args: [FEE_VAULT_ADDRESS],
   });
+
+  const configurationContract = new ethers.Contract(configuration.address, configuration.abi, signer);
+  if (!(await configurationContract.paymentTokenWhitelist(WCHZ))) {
+    const tx1 = await configurationContract.allowTokenForPayment(WCHZ);
+    await tx1.wait();
+  }
 
   console.log(`Configuration is deployed(${configuration.address})`);
 };
