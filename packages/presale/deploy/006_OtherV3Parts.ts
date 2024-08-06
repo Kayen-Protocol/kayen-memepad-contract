106773cbd148ff896750617624e16b40585fb50b
boxfox619 <boxfoxsg619@gmail.com>
2024-08-06 10:34:01 +0900

    Redeploy

--- packages/presale/deploy/006_OtherV3Parts.ts
+++ packages/presale/deploy/006_OtherV3Parts.ts
@@ -1,8 +1,7 @@
 import { utils } from "ethers";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-
-const WCHZ = "0x678c34581db0a7808d0aC669d7025f1408C9a3C6";
+import { WCHZ } from "./constants";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
