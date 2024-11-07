44b3540496541e1b9f2c969bfbd8801ff523d024
0xdaniel_333 <63653067+thisisdaniel333@users.noreply.github.com>
2024-11-07 16:29:20 +0900

    Feat: Preparation for multichain deployments

--- packages/presale/deploy/006_OtherV3Parts.ts
+++ packages/presale/deploy/006_OtherV3Parts.ts
@@ -1,35 +1,38 @@
 import { utils } from "ethers";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { WCHZ } from "./constants";
+import { getNetworkAddresses } from "./constants";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
+  const { deployments, getNamedAccounts, ethers} = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
+  const { chainId } = await ethers.provider.getNetwork();
+
+  const { wETH } = getNetworkAddresses(chainId);
 
   const poolFactory = await deployments.get("UniswapV3Factory");
   const descriptor = await deploy("NonfungibleTokenPositionDescriptor", {
     from: deployer,
-    args: [WCHZ, utils.formatBytes32String("WCHZ")],
+    args: [wETH, utils.formatBytes32String("wETH")],
   });
   console.log(`NonfungibleTokenPositionDescriptor is deployed(${descriptor.address})`);
 
   const positionManager = await deploy("NonfungiblePositionManager", {
     from: deployer,
-    args: [poolFactory.address, WCHZ, descriptor.address],
+    args: [poolFactory.address, wETH, descriptor.address],
   });
   console.log(`NonfungiblePositionManager is deployed(${positionManager.address})`);
 
   const swapRouter = await deploy("SwapRouter", {
     from: deployer,
-    args: [poolFactory.address, WCHZ],
+    args: [poolFactory.address, wETH],
   });
   console.log(`SwapRouter is deployed(${swapRouter.address})`);
 
   const quoter = await deploy("Quoter", {
     from: deployer,
-    args: [poolFactory.address, WCHZ],
+    args: [poolFactory.address, wETH],
   });
 
   console.log(`Quoter is deployed(${quoter.address})`);
