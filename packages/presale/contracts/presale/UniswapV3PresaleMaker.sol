ac3c53ebef0bd80d9a5ea895e20f87863abac42a
0xdaniel_333 <63653067+thisisdaniel333@users.noreply.github.com>
2024-11-16 22:00:37 +0900

    Fix: audit fix for xeal

--- packages/presale/contracts/presale/UniswapV3PresaleMaker.sol
+++ packages/presale/contracts/presale/UniswapV3PresaleMaker.sol
@@ -202,6 +202,10 @@ contract UniswapV3PresaleMaker is ERC721Receiver {
             startTimestamp == 0 || startTimestamp >= block.timestamp,
             "UniswapV3PresaleMaker: start timestamp cannot be in the past"
         );
+        // if contract is not whitelisted, minter can be different from msg.sender
+        if (!config.isWhitelistedContract(address(this))) {
+            require(minter == msg.sender, "UniswapV3PresaleMaker: minter must be msg.sender");
+        }
 
         address _paymentToken = paymentToken;
         if (eth == paymentToken) {
